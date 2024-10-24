4520cef8b5aed5e681dceec386d7f5570e3c52b1
ld051609 <landinhhoang0509@gmail.com>
2024-10-24 13:06:39 -0400

    finish a2

--- finalA2/q2/q2.c
+++ finalA2/q2/q2.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 int main(int argc, char** argv){
 
   //Pointer to the stack
@@ -18,9 +17,37 @@ int main(int argc, char** argv){
     
     /* **Hint** You will need to perform stack operations that 
     we implemented here to achieve the expected output */
-    
- 
+
+    // addition here
+    if(argv[1][i] == '+'){
+      double a = pop(&stack);
+      double b = pop(&stack);
+      push(&stack, a + b);
+    }
+    // subtraction here
+    else if(argv[1][i] == '-'){
+      double a = pop(&stack);
+      double b = pop(&stack);
+      push(&stack, b - a);
+    }
+    // multiplication here
+    else if(argv[1][i] == '*'){
+      double a = pop(&stack);
+      double b = pop(&stack);
+      push(&stack, a * b);
+    }
+    // division here
+    else if(argv[1][i] == '/'){
+      double a = pop(&stack);
+      double b = pop(&stack);
+      push(&stack, b / a);
+    }
+    // If the character is a number, push it onto the stack here
+    else if(argv[1][i] != ' '){
+      push(&stack, argv[1][i] - '0');
+    }
   }
+  printf("The result is: %.2f\n", pop(&stack));
   
   
 
--- finalA2/q2/q2_functions.c
+++ finalA2/q2/q2_functions.c
@@ -15,6 +15,11 @@ Assignment 2, Question 2
  * @param value The value to be added to the stack.
  */
 void push(Operand **stack, double value){
+  Operand *newNode = (Operand *)malloc(sizeof(Operand));
+  newNode->value = value; //give the new node the value
+  newNode->next = *stack; //point the new node to the current head
+  *stack = newNode; //make the new node the head
+
 
 }
 
@@ -26,6 +31,14 @@ void push(Operand **stack, double value){
  * @return The value that was at the top of the stack.
  */
 double pop(Operand **stack){
+  if(*stack == NULL){
+    return 0;
+  }
+  Operand *temp = *stack; //create a temporary pointer to the head
+  double value = temp->value; //store the value of the head
+  *stack = temp->next; //move the head to the next node
+  free(temp); //free the memory of the old head
+  return value; //return the value of the old head
 
 }
 
@@ -36,5 +49,10 @@ double pop(Operand **stack){
  * @param stack Pointer to the stack's head.
  */
 void print(Operand *stack){
+  Operand *temp = stack; //create a temporary pointer to the head
+  while(temp != NULL){ //while the pointer is not null
+    printf("%lf\n", temp->value); //print the value of the node
+    temp = temp->next; //move to the next node
+  }
   
 }
